ce45fed50c8ea406bbec1546a22e8c500ce480fd
mathengejob <jobmathenge90@gmail.com>
2022-03-18 12:44:03 +0300

    v1-track commit 1

--- Archives/firmware_checker/track.ino
+++ Archives/firmware_checker/track.ino
@@ -22,7 +22,7 @@ digitalWrite(rmotorf,HIGH);
 digitalWrite(lmotorb,LOW);
 digitalWrite(rmotorb,LOW);
 }
-
+//>>>>>>>>>>>>>>>right
 else if((sensor1==0)&&(sensor2==1)&&(sensor3==1)&&(sensor4==0)&&(sensor5==0))// slight to right>> move foward)
 { 
  analogWrite(l_pwm, l_max_speedf-20);
@@ -32,6 +32,46 @@ digitalWrite(rmotorf,HIGH);
 digitalWrite(lmotorb,LOW);
 digitalWrite(rmotorb,LOW);
 }
+
+else if((sensor1==0)&&(sensor2==1)&&(sensor3==0)&&(sensor4==0)&&(sensor5==0))// far to the right>> turn left
+{ 
+ analogWrite(l_pwm, l_max_speedf-20);//30
+analogWrite(r_pwm, r_max_speedf);//30
+digitalWrite(lmotorf,HIGH);
+digitalWrite(rmotorf,HIGH);
+digitalWrite(lmotorb,LOW);
+digitalWrite(rmotorb,LOW);
+}
+else if((sensor1==1)&&(sensor2==1)&&(sensor3==1)&&(sensor4==0)&&(sensor5==0))// far to the right>> turn left
+{ 
+ analogWrite(l_pwm, l_max_speedf-20);//30
+analogWrite(r_pwm, r_max_speedf);//30
+digitalWrite(lmotorf,HIGH);///  WAS at lows
+digitalWrite(rmotorf,HIGH);
+digitalWrite(lmotorb,LOW);
+digitalWrite(rmotorb,LOW);
+}
+else if((sensor1==1)&&(sensor2==1)&&(sensor3==0)&&(sensor4==0)&&(sensor5==0))// far to the right>> turn left
+{ 
+ analogWrite(l_pwm, l_max_speedf-20);//30
+analogWrite(r_pwm, r_max_speedf);//30
+digitalWrite(lmotorf,LOW);
+digitalWrite(rmotorf,HIGH);
+digitalWrite(lmotorb,LOW);
+digitalWrite(rmotorb,LOW);
+}
+else if((sensor1==1)&&(sensor2==0)&&(sensor3==0)&&(sensor4==0)&&(sensor5==0))// far to the right>> turn left
+{ 
+ analogWrite(l_pwm, l_max_speedf-20);//30
+analogWrite(r_pwm, r_max_speedf+20);//30
+digitalWrite(lmotorf,LOW);
+digitalWrite(rmotorf,HIGH);
+digitalWrite(lmotorb,LOW);
+digitalWrite(rmotorb,LOW);
+pre_l_value=cur_l_value;
+pre_r_value=cur_r_value;
+}
+//>>>>>>>>>>>>>>>>>>> Left
 else if((sensor1==0)&&(sensor2==0)&&(sensor3==1)&&(sensor4==1)&&(sensor5==0))// slight to the left >>move forward
 { 
  analogWrite(l_pwm, l_max_speedf+0);
@@ -41,66 +81,28 @@ digitalWrite(rmotorf,HIGH);
 digitalWrite(lmotorb,LOW);
 digitalWrite(rmotorb,LOW);
 }
-else if((sensor1==0)&&(sensor2==1)&&(sensor3==0)&&(sensor4==0)&&(sensor5==0))// far to the right>> turn left
-{ 
- analogWrite(l_pwm, l_max_speedf);//30
-analogWrite(r_pwm, r_max_speedf);//30
-digitalWrite(lmotorf,LOW);
-digitalWrite(rmotorf,HIGH);
-digitalWrite(lmotorb,LOW);
-digitalWrite(rmotorb,LOW);
-}
-else if((sensor1==1)&&(sensor2==1)&&(sensor3==0)&&(sensor4==0)&&(sensor5==0))// far to the right>> turn left
-{ 
- analogWrite(l_pwm, l_max_speedf+30);//30
-analogWrite(r_pwm, r_max_speedf+30);//30
-digitalWrite(lmotorf,LOW);
-digitalWrite(rmotorf,HIGH);
-digitalWrite(lmotorb,LOW);
-digitalWrite(rmotorb,LOW);
-}
-else if((sensor1==1)&&(sensor2==1)&&(sensor3==1)&&(sensor4==0)&&(sensor5==0))// far to the right>> turn left
-{ 
- analogWrite(l_pwm, l_max_speedf+30);//30
-analogWrite(r_pwm, r_max_speedf+30);//30
-digitalWrite(lmotorf,LOW);
-digitalWrite(rmotorf,HIGH);
-digitalWrite(lmotorb,LOW);
-digitalWrite(rmotorb,LOW);
-}
-else if((sensor1==1)&&(sensor2==0)&&(sensor3==0)&&(sensor4==0)&&(sensor5==0))// far to the right>> turn left
-{ 
- analogWrite(l_pwm, l_max_speedf+30);//30
-analogWrite(r_pwm, r_max_speedf+30);//30
-digitalWrite(lmotorf,LOW);
-digitalWrite(rmotorf,HIGH);
-digitalWrite(lmotorb,LOW);
-digitalWrite(rmotorb,LOW);
-pre_l_value=cur_l_value;
-pre_r_value=cur_r_value;
-}
-else if((sensor1==1)&&(sensor2==0)&&(sensor3==0)&&(sensor4==1)&&(sensor5==0))//far to the left >> right turn
+else if((sensor1==0)&&(sensor2==0)&&(sensor3==0)&&(sensor4==1)&&(sensor5==0))//far to the left >> right turn
 { 
  analogWrite(l_pwm, l_max_speedf+0);//30//50
-analogWrite(r_pwm, r_max_speedf+0);//30//50
+analogWrite(r_pwm, r_max_speedf-20);//30//50
 digitalWrite(lmotorf,HIGH);
-digitalWrite(rmotorf,LOW);
+digitalWrite(rmotorf,HIGH);
+digitalWrite(lmotorb,LOW);
+digitalWrite(rmotorb,LOW);
+}
+else if((sensor1==0)&&(sensor2==0)&&(sensor3==1)&&(sensor4==1)&&(sensor5==1))//far to the left >> right turn
+{ 
+ analogWrite(l_pwm, l_max_speedf);//30//50
+analogWrite(r_pwm, r_max_speedf-20);//30//50
+digitalWrite(lmotorf,HIGH);
+digitalWrite(rmotorf,HIGH);
 digitalWrite(lmotorb,LOW);
 digitalWrite(rmotorb,LOW);
 }
 else if((sensor1==0)&&(sensor2==0)&&(sensor3==0)&&(sensor4==1)&&(sensor5==1))//far to the left >> right turn
 { 
- analogWrite(l_pwm, l_max_speedf+30);//30//50
-analogWrite(r_pwm, r_max_speedf+30);//30//50
-digitalWrite(lmotorf,HIGH);
-digitalWrite(rmotorf,LOW);
-digitalWrite(lmotorb,LOW);
-digitalWrite(rmotorb,LOW);
-}
-else if((sensor1==0)&&(sensor2==0)&&(sensor3==0)&&(sensor4==1)&&(sensor5==1))//far to the left >> right turn
-{ 
- analogWrite(l_pwm, l_max_speedf+30);//30//50
-analogWrite(r_pwm, r_max_speedf+30);//30//50
+ analogWrite(l_pwm, l_max_speedf+20);//30//50
+analogWrite(r_pwm, r_max_speedf-20);//30//50
 digitalWrite(lmotorf,HIGH);
 digitalWrite(rmotorf,LOW);
 digitalWrite(lmotorb,LOW);
@@ -108,8 +110,8 @@ digitalWrite(rmotorb,LOW);
 }
 else if((sensor1==0)&&(sensor2==0)&&(sensor3==0)&&(sensor4==0)&&(sensor5==1))//far to the left >> right turn
 { 
- analogWrite(l_pwm, l_max_speedf+30);//30//50
-analogWrite(r_pwm, r_max_speedf+30);//30//50
+ analogWrite(l_pwm, l_max_speedf+20);//30//50
+analogWrite(r_pwm, r_max_speedf-20);//30//50
 digitalWrite(lmotorf,HIGH);
 digitalWrite(rmotorf,LOW);
 digitalWrite(lmotorb,LOW);
