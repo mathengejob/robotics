97a755307546f1205c9cdbf8230a0b73772d82d1
G-kairu <rkairu68@gmail.com>
2022-03-23 18:01:03 +0300

    archive code for regionals

--- FinalCode/FinalCode.ino
+++ FinalCode/FinalCode.ino
@@ -9,8 +9,8 @@ VarSpeedServo servo4; // base servo
 #define model 1080   
 SharpIR mySensor = SharpIR(IRPin,model); 
 /* Model :
-
-  GP2Y0A02YK0F --> 20150
+  
+     GP2Y0A02YK0F --> 20150
   GP2Y0A21YK0F --> 1080
   GP2Y0A710K0F --> 100500
   GP2YA41SK0F --> 430
